925f32ef5e561957b7fcf68ae7ed223a2a9cd40c
karankiri <karan.kiri@fabric.inc>
2022-09-05 13:30:38 +0530

    initial code

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,25 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+let grandTotal = 0;
+const totalOrder = parseInt(
+  window.document.querySelectorAll('span.num-orders')[0].innerText
+);
+const orders = window.document.querySelectorAll('.yohtmlc-order-total .value');
+const pageTotal = Array.from(orders).reduce((acc, curr) => {
+  return parseFloat(curr.innerText.replaceAll(',', '')) + acc;
+}, 0);
+console.log(pageTotal);
+grandTotal += pageTotal;
+for (let i = 0; i < totalOrder / 10; i++) {
+  const nextButton = document.querySelector('ul.a-pagination li.a-last a');
+  nextButton.click();
+  const orders = window.document.querySelectorAll(
+    '.yohtmlc-order-total .value'
+  );
+  const pageTotal = Array.from(orders).reduce((acc, curr) => {
+    return parseFloat(curr.innerText.replaceAll(',', '')) + acc;
+  }, 0);
+  console.log(pageTotal);
+  grandTotal += pageTotal;
+}
+console.log('grandTotal', grandTotal);
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
+  const handleCalculate = () => {
+    console.log('hello');
+    chrome.tabs.query(
+      { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+      function (tabs) {
+        console.log(
+          '🚀 ~ file: Popup.jsx ~ line 12 ~ handleCalculate ~ tabs',
+          tabs
+        );
+        const { id: tabId } = tabs[0];
+
+        let code = `window.document.querySelectorAll(
+          '.yohtmlc-order-total .value'
+        )`;
+
+        // http://infoheap.com/chrome-extension-tutorial-access-dom/
+        chrome.tabs.executeScript(tabId, { code }, function (result) {
+          console.log('🚀 ~ file: Popup.jsx ~ line 20 ~ result', result);
+          // result has the return value from `code`
+        });
+      }
+    );
+    const orders = window.document.querySelectorAll(
+      '.yohtmlc-order-total .value'
+    );
+    console.log(
+      '🚀 ~ file: Popup.jsx ~ line 9 ~ handleCalculate ~ orders',
+      orders
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleCalculate}>Calculate</button>
         <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
         </p>
